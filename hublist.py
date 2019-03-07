217f3bb1871516f2e3ed3f713b0778d0ae8ea98e
Kcchouette <Kcchouette@users.noreply.github.com>
2019-03-07 14:01:24 +0100

    integrate dcping

--- hublist.py
+++ hublist.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import bz2
-import urllib, urllib.request, urllib.parse
 import sys
+import os
+import bz2
+import socket
+import urllib, urllib.request, urllib.parse
 import xml.etree.cElementTree as ET
 import socket
+#from subprocess import Popen, PIPE
+from subprocess import check_output, run, PIPE
+
+# List of attributes, in the form (attribute name, default value, type)
+attributes = (
+        # useful for all
+        ('Address', '', 'string'),
+        ('Name', '', 'string'),
+        ('Description', '', 'string'),
+        ('Users', 0, 'int'),
+        ('Country', '', 'string'),
+        ('Shared', 0, 'bytes'),
+        ('Minshare', 0, 'bytes'),
+        ('Minslots', 0, 'int'),
+        ('Maxhubs', 0, 'int'),
+        ('Maxusers', 0, 'int'),
+        ('Reliability', 0, 'string'),
+        ('Rating', '0', 'string'),
+        # normally useful, for NMDC
+        ('Encoding', '', 'string'),
+        # useful for flylinkdc
+        ('Software', '', 'string'),
+        ('Website', '', 'string'),
+        ('Email', '', 'string'),
+        ('ASN', '', 'string'),
+        ('Operators', 0, 'int'),
+        ('Bots', 0, 'int'),
+        ('Infected', 0, 'int'),
+        # useful for website
+        ('Status', 'Offline', 'string'),
+        # useful for this script
+        ('Failover', '', 'string'),
+)
 
 # in-place prettyprint formatter from http://effbot.org/zone/element-lib.htm#prettyprint
 def indent(elem, level=0):
@@ -38,7 +73,16 @@ def get_host_ip(hostname, fallback=None):
     ip_addr = socket.gethostbyname(hostname)
     return ip_addr
 
-urls = [ 
+def hub_merge(hub1, hub2):
+    """ Set attributes with no value in hub1 from value in hub2 """
+    for att, _, _ in attributes:
+        if att in hub2.attrib:
+            if hub1.attrib.get(att) == None and hub2.attrib.get(att) != None:
+                hub1.attrib[att] = hub2.attrib[att]
+    return hub1
+
+
+external_hublist = [
     # list based on:
     # - https://sourceforge.net/p/dcplusplus/code/ci/default/tree/dcpp/SettingsManager.cpp#l197
     # - https://github.com/airdcpp/airgit/blob/master/airdcpp/airdcpp/SettingsManager.cpp#L339
@@ -47,79 +91,86 @@ urls = [
     "http://dchublist.org/hublist.xml.bz2",
     "https://dchublist.ru/hublist.xml.bz2",
     #"http://hublist.eu/hublist.xml.bz2", # cloudflare
-    #"http://dchublists.com/?do=hublist&get=hublist.xml", # unknown error
+    "http://dchublists.com/?do=hublist&get=hublist.xml",
     #"http://www.hublista.hu/hublist.xml.bz2", # too many timeout
-    "http://dchublist.biz/?do=hublist.xml.bz2",
+    #"http://dchublist.biz/?do=hublist.xml.bz2", # unknown error
 ]
+# timeout in seconds
+timeout = 10
+socket.setdefaulttimeout(timeout)
+
 xml_files = []
 
 # Download files (and extract if necessary)
-for url in urls:
+for url in external_hublist:
     print('Will download hub list from', url)
     xml_file = urllib.request.urlopen(url).read()
     if url.endswith('.bz2'):
         xml_file = bz2.decompress(xml_file)
     xml_files.append(xml_file)
 
-# Parsing XML files
-colsets = []
-hubs = dict()
-for xml_file, url in zip(xml_files, urls):
-    root = ET.fromstring(xml_file)
-    cols = list(root.iter('Column'))
-    colset = set()
-    for c in cols:
-        colset.add((c.attrib['Name'], c.attrib['Type']))
-    colsets.append(colset)
+# checking dcping command is send
+if len(sys.argv) != 2:
+    print("Usage: python hublist.py [location_of_dcping]")
+    sys.exit (1)
 
-    for hub in root.iter('Hub'): 
+# Parsing XML files
+hubs = []
+
+for xml_file in xml_files:
+    root = ET.fromstring(xml_file)
+
+    for hub in root.iter('Hub'):
 
         # Add DCHUB protocol to url if no protocol is specified
         if not urllib.parse.urlparse(hub.attrib['Address']).scheme:
             print('Adding dchub:// to hub adress with unspecified protocol', hub.attrib['Address'])
-            hub.attrib['Address'] = "dchub://" + hub.attrib['Address']
+            hub.attrib['Address'] = 'dchub://' + hub.attrib['Address']
 
-        # Add DCHUB optional port to url if no port is specified
-        if urllib.parse.urlparse(hub.attrib['Address']).scheme == "dchub" and not urllib.parse.urlparse(hub.attrib['Address']).port:
+        # Add NMDC optional port to url if no port is specified
+        if urllib.parse.urlparse(hub.attrib['Address']).scheme == 'dchub' and not urllib.parse.urlparse(hub.attrib['Address']).port:
             print('Adding :411 to hub adress with unspecified port', hub.attrib['Address'])
-            hub.attrib['Address'] = hub.attrib['Address'] + ":411"
+            hub.attrib['Address'] = hub.attrib['Address'] + ':411'
+        
+        hubs.append(hub)
 
-        # Remove duplicate hubs
-        try:
-            ip = get_host_ip(urllib.parse.urlparse(hub.attrib['Address']).hostname)
-            hub_adr = urllib.parse.urlparse(hub.attrib['Address']).scheme + "://" + ip + ":" + str(urllib.parse.urlparse(hub.attrib['Address']).port)
-            if hub_adr in hubs.keys():
-                print('Dropping duplicate hub', hub_adr, 'from hub list', url)
-                for key in hubs[hub_adr].keys():
-                    if key not in hubs[hub_adr].keys():
-                        hubs[hub_adr][key] = hub[key]
-                continue
-        except:
-            print(hub.attrib['Address'], 'does not respond to ping, IGNORED')
-            continue
-        hubs[hub_adr] = hub
+clean_hubs = []
+DEVNULL = open(os.devnull, 'wb')
+while len(hubs) != 0:
+    hub = hubs[0]
 
-# Keep all colums encountered
-colnames = set.union(*colsets)
-print('Columns in output file:', [c[0] for c in colnames])
+    #output = check_output([sys.argv[1], 'ping', hub.attrib['Address'], '--out=xml'], stderr=DEVNULL)
+    output = run([sys.argv[1], 'ping', hub.attrib['Address'], '--out=xml'], check=False, stdout=PIPE).stdout
+
+    hub_response = ET.fromstring(output)
+
+    print(hub_response.attrib['Address'])
+    duplicata_hubs = [ h for h in hubs if h.attrib['Address'] in (hub_response.attrib['Address'], hub_response.attrib.get('Failover')) ]
+
+    for duplicata_hub in duplicata_hubs: 
+        hub_response = hub_merge(hub_response, duplicata_hub)
+
+    clean_hubs.append(hub_response)
+    hubs = [ h for h in hubs if h.attrib['Address'] not in (hub_response.attrib['Address'], hub_response.attrib.get('Failover')) ]
 
 # Prepare output file
 merge_root = ET.Element('Hublist')
 merge_hubs = ET.SubElement(merge_root, 'Hubs')
 merge_cols = ET.SubElement(merge_hubs, 'Columns')
-for cn in colnames:
-    name, type_ = cn
+
+# do columns
+for name, _, type_ in attributes:
     ET.SubElement(merge_cols, 'Column', Name=name, Type=type_)
 
-for hub_adr, hub_elem in hubs.items():
+# poplate hub columns
+for hub_add in clean_hubs:
     attribs = {}
-    for k in colnames:
-        c, type_ = k
-        if c in hub_elem.attrib:
-            attribs[c] = hub_elem.attrib[c]
+    for name, default, type_ in attributes:
+        if name in hub_add.attrib and hub_add.attrib.get(name) != None:
+            attribs[name] = hub_add.attrib.get(name)
         else:
             # Inserting default value
-            attribs[c] = ''
+            attribs[name] = str(default)
     ET.SubElement(merge_hubs, 'Hub', attribs)
 
 indent(merge_root)
@@ -128,6 +179,6 @@ merge_tree.write('hublist.xml', encoding='UTF-8', xml_declaration=True)
 
 # bz2
 tarbz2contents = bz2.compress(open('hublist.xml', 'rb').read(), 9)
-fh = open('hublist.xml.bz2', "wb")
+fh = open('hublist.xml.bz2', 'wb')
 fh.write(tarbz2contents)
 fh.close()
