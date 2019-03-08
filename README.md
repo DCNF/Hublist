acac32e62ebe5afd0b3595f7ab925a12dd50b596
Kcchouette <Kcchouette@users.noreply.github.com>
2019-03-08 20:41:09 +0100

    Integrate dcping (#4)
    * Update README.md
    * integrate dcping
    * refactor some things
    * Update README.md
    * Fix issue with local file
    * Update hublist.xml
    * Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # Hublist
 
+## How to use hublist.py?
+
+The pinger helps us having more accurate data of an hub. 
+
+### With the pinger DCPing
+
+Version 0.8.8 of dcping works with our script (`a6e91ec`).
+
+```
+$ python3 hublist.py "[folder_to_dcping]/dcping"
+```
+
+### Without the pinger
+
+```
+$ python3 hublist.py
+```
+
+### `DCPing`?
+
+**DCPing** is an open-source pinger build on GitHub: https://github.com/direct-connect/go-dcpp/tree/master/cmd/dcping.  
+You can have a functionnal version by [building its source](https://github.com/direct-connect/go-dcpp/tree/master/cmd/dcping#build).
+
+## Which attribute is used?
+
 - https://sourceforge.net/p/dcplusplus/code/ci/default/tree/dcpp/FavoriteManager.cpp#l322
 - https://github.com/airdcpp/airgit/blob/master/airdcpp/airdcpp/modules/HublistManager.cpp#L58
 - https://github.com/pavel-pimenov/flylinkdc-r5xx/blob/master/windows/PublicHubsFrm.cpp#L56
+
+## License
+
+[GPLv2 or later](https://github.com/DCNF/Hublist/blob/master/LICENSE)
--- hublist.py
+++ hublist.py
@@ -2,10 +2,66 @@
 # -*- coding: utf-8 -*-
 
 import bz2
-import urllib, urllib.request, urllib.parse
-import sys
-import xml.etree.cElementTree as ET
 import socket
+import sys
+import urllib, urllib.request, urllib.parse
+import xml.etree.cElementTree as ET
+from subprocess import run, PIPE
+
+##### CAN BE CONFIGURED #####
+internet_hublists = [
+    # list based on:
+    # - https://sourceforge.net/p/dcplusplus/code/ci/default/tree/dcpp/SettingsManager.cpp#l197
+    # - https://github.com/airdcpp/airgit/blob/master/airdcpp/airdcpp/SettingsManager.cpp#L339
+    # - https://github.com/pavel-pimenov/flylinkdc-r5xx/blob/master/compiled/Settings/flylinkdc-config-r5xx.xml#L22
+    "http://www.te-home.net/?do=hublist&get=hublist.xml",
+    "http://dchublist.org/hublist.xml.bz2",
+    "https://dchublist.ru/hublist.xml.bz2",
+    #"http://hublist.eu/hublist.xml.bz2", # cloudflare
+    "http://dchublists.com/?do=hublist&get=hublist.xml",
+    #"http://www.hublista.hu/hublist.xml.bz2", # too many timeout
+    #"http://dchublist.biz/?do=hublist.xml.bz2", # unknown error
+]
+
+local_hublists = [
+    #"/home/user/file.xml",
+]
+
+# timeout in seconds
+timeout = 10
+socket.setdefaulttimeout(timeout)
+##### END OF THE CONFIGURATION #####
+
+# List of attributes, in the form (attribute name, type)
+attributes = (
+        # useful for all
+        ('Address', 'string'),
+        ('Name', 'string'),
+        ('Description', 'string'),
+        ('Users', 'int'),
+        ('Country', 'string'),
+        ('Shared', 'bytes'),
+        ('Minshare', 'bytes'),
+        ('Minslots', 'int'),
+        ('Maxhubs', 'int'),
+        ('Maxusers', 'int'),
+        ('Reliability', 'string'),
+        ('Rating', 'string'),
+        # normally useful, for NMDC
+        ('Encoding', 'string'),
+        # useful for flylinkdc
+        ('Software', 'string'),
+        ('Website', 'string'),
+        ('Email', 'string'),
+        ('ASN', 'string'),
+        ('Operators', 'int'),
+        ('Bots', 'int'),
+        ('Infected', 'int'),
+        # useful for website
+        ('Status', 'string'),
+        # useful for this script
+        ('Failover', 'string'),
+)
 
 # in-place prettyprint formatter from http://effbot.org/zone/element-lib.htm#prettyprint
 def indent(elem, level=0):
@@ -23,103 +79,92 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-# based on https://github.com/openstack/charm-plumgrid-gateway/blob/master/hooks/charmhelpers/contrib/network/ip.py
-def is_ip(address):
-    try:
-        # Test to see if already an IPv4 address
-        socket.inet_aton(address)
-        return True
-    except socket.error:
-        return False
+def hub_merge(hub1, hub2):
+    # Set attributes with no value in hub1 from value in hub2
+    for att, _ in attributes:
+        if att in hub2.attrib:
+            if hub1.attrib.get(att) == None and hub2.attrib.get(att) != None:
+                hub1.attrib[att] = hub2.attrib[att]
+    return hub1
 
-def get_host_ip(hostname, fallback=None):
-    if is_ip(hostname):
-        return hostname
-    ip_addr = socket.gethostbyname(hostname)
-    return ip_addr
-
-urls = [ 
-    # list based on:
-    # - https://sourceforge.net/p/dcplusplus/code/ci/default/tree/dcpp/SettingsManager.cpp#l197
-    # - https://github.com/airdcpp/airgit/blob/master/airdcpp/airdcpp/SettingsManager.cpp#L339
-    # - https://github.com/pavel-pimenov/flylinkdc-r5xx/blob/master/compiled/Settings/flylinkdc-config-r5xx.xml#L22
-    "http://www.te-home.net/?do=hublist&get=hublist.xml",
-    "http://dchublist.org/hublist.xml.bz2",
-    "https://dchublist.ru/hublist.xml.bz2",
-    #"http://hublist.eu/hublist.xml.bz2", # cloudflare
-    #"http://dchublists.com/?do=hublist&get=hublist.xml", # unknown error
-    #"http://www.hublista.hu/hublist.xml.bz2", # too many timeout
-    "http://dchublist.biz/?do=hublist.xml.bz2",
-]
 xml_files = []
 
 # Download files (and extract if necessary)
-for url in urls:
+for url in internet_hublists:
     print('Will download hub list from', url)
     xml_file = urllib.request.urlopen(url).read()
     if url.endswith('.bz2'):
         xml_file = bz2.decompress(xml_file)
     xml_files.append(xml_file)
 
-# Parsing XML files
-colsets = []
-hubs = dict()
-for xml_file, url in zip(xml_files, urls):
-    root = ET.fromstring(xml_file)
-    cols = list(root.iter('Column'))
-    colset = set()
-    for c in cols:
-        colset.add((c.attrib['Name'], c.attrib['Type']))
-    colsets.append(colset)
+# Import local file
+for local_hublist in local_hublists:
+    print('Loading hub list from', local_hublist)
+    if local_hublist.endswith('.bz2'):
+        local_hublist = bz2.BZ2File(local_hublist)
+    root = ET.parse(local_hublist).getroot()
+    xml_files.append(ET.tostring(root).decode())
 
-    for hub in root.iter('Hub'): 
+# Parsing XML files
+hubs = []
+
+for xml_file in xml_files:
+    root = ET.fromstring(xml_file)
+
+    for hub in root.iter('Hub'):
 
         # Add DCHUB protocol to url if no protocol is specified
         if not urllib.parse.urlparse(hub.attrib['Address']).scheme:
             print('Adding dchub:// to hub adress with unspecified protocol', hub.attrib['Address'])
-            hub.attrib['Address'] = "dchub://" + hub.attrib['Address']
+            hub.attrib['Address'] = 'dchub://' + hub.attrib['Address']
 
-        # Add DCHUB optional port to url if no port is specified
-        if urllib.parse.urlparse(hub.attrib['Address']).scheme == "dchub" and not urllib.parse.urlparse(hub.attrib['Address']).port:
+        # Add NMDC optional port to url if no port is specified
+        if urllib.parse.urlparse(hub.attrib['Address']).scheme == 'dchub' and not urllib.parse.urlparse(hub.attrib['Address']).port:
             print('Adding :411 to hub adress with unspecified port', hub.attrib['Address'])
-            hub.attrib['Address'] = hub.attrib['Address'] + ":411"
+            hub.attrib['Address'] = hub.attrib['Address'] + ':411'
 
-        # Remove duplicate hubs
-        try:
-            ip = get_host_ip(urllib.parse.urlparse(hub.attrib['Address']).hostname)
-            hub_adr = urllib.parse.urlparse(hub.attrib['Address']).scheme + "://" + ip + ":" + str(urllib.parse.urlparse(hub.attrib['Address']).port)
-            if hub_adr in hubs.keys():
-                print('Dropping duplicate hub', hub_adr, 'from hub list', url)
-                for key in hubs[hub_adr].keys():
-                    if key not in hubs[hub_adr].keys():
-                        hubs[hub_adr][key] = hub[key]
-                continue
-        except:
-            print(hub.attrib['Address'], 'does not respond to ping, IGNORED')
-            continue
-        hubs[hub_adr] = hub
+        hubs.append(hub)
 
-# Keep all colums encountered
-colnames = set.union(*colsets)
-print('Columns in output file:', [c[0] for c in colnames])
+clean_hubs = []
+
+while len(hubs) != 0:
+    hub = hubs[0]    
+
+    if len(sys.argv) >= 2:
+        output = run([sys.argv[1], 'ping', hub.attrib['Address'], '--out=xml'], check=False, stdout=PIPE).stdout
+        hub_response = ET.fromstring(output)
+        print(hub_response.attrib['Address'])
+        if hub_response.attrib['Status'] == 'Error':
+            hub_response = hub
+    else:
+        hub_response = hub
+
+    duplicata_hubs = [ h for h in hubs if h.attrib['Address'] in (hub_response.attrib['Address'], hub_response.attrib.get('Failover')) ]
+
+    for duplicata_hub in duplicata_hubs: 
+        hub_response = hub_merge(hub_response, duplicata_hub)
+
+    clean_hubs.append(hub_response)
+    hubs = [ h for h in hubs if h.attrib['Address'] not in (hub_response.attrib['Address'], hub_response.attrib.get('Failover')) ]
 
 # Prepare output file
-merge_root = ET.Element('Hublist')
+merge_root = ET.Element('Hublist', Name='The DCNF Hublist', Address='https://dcnf.github.io/Hublist/')
 merge_hubs = ET.SubElement(merge_root, 'Hubs')
 merge_cols = ET.SubElement(merge_hubs, 'Columns')
-for cn in colnames:
-    name, type_ = cn
+
+# do columns
+for name, type_ in attributes:
     ET.SubElement(merge_cols, 'Column', Name=name, Type=type_)
 
-for hub_adr, hub_elem in hubs.items():
+# populate hub columns
+for hub_add in clean_hubs:
     attribs = {}
-    for k in colnames:
-        c, type_ = k
-        if c in hub_elem.attrib:
-            attribs[c] = hub_elem.attrib[c]
+    for name, _ in attributes:
+        if name in hub_add.attrib and hub_add.attrib.get(name) != None:
+            attribs[name] = hub_add.attrib.get(name)
         else:
-            # Inserting default value
-            attribs[c] = ''
+            # Inserting no value
+            attribs[name] = ''
     ET.SubElement(merge_hubs, 'Hub', attribs)
 
 indent(merge_root)
@@ -128,6 +173,6 @@ merge_tree.write('hublist.xml', encoding='UTF-8', xml_declaration=True)
 
 # bz2
 tarbz2contents = bz2.compress(open('hublist.xml', 'rb').read(), 9)
-fh = open('hublist.xml.bz2', "wb")
+fh = open('hublist.xml.bz2', 'wb')
 fh.write(tarbz2contents)
 fh.close()
